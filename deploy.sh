8959eb9b89f919ee1c57db6af88b97187334ce8a
BigMacCheese <juandamor45@gmail.com>
2024-11-21 12:34:11 -0500

    Fix deployment commands

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
-npx hardhat compile
+# When using ignition there is no need to compile
+# Uncomment if you want to compile
 
-npx hardhat ignition deploy ./ignition/modules/Lock.js --network localhost
+# npx hardhat compile
+
+# For deployment in besu network this is not longer used
+
+# npx hardhat ignition deploy ./ignition/modules/Lock.js --network localhost
+
+npx hardhat ignition deploy ./ignition/modules/Lock.js --network besu
+
+npx hardhat ignition deploy ./ignition/modules/StableCoinDeploy.js --network besu
