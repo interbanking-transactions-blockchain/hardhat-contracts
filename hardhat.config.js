f5d0f73f6311923211c4bc498571d8c790477381
BigMacCheese <juandamor45@gmail.com>
2024-11-21 12:31:12 -0500

    Modified hardhat.config for connection to besu network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+// besuUrl is the url where the bootnode a is deployed
+const besuUrl = process.env.BESU_URL_NODE_A;
+
+// privateKey is the privateKey of the bootnode a which is the one that sign the tx that deploy the smart contract
+const privateKey = process.env.PRIVATE_KEY_BOOTNODE_A;
+
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    besu: {
+      url: besuUrl,
+      accounts: [privateKey],
+    },
+  },
 };
